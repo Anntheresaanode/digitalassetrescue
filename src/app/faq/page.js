5ede91b7f0486306aa1fbc6eecbd1c2106cc9027
Anntheresaanode <annatheresanode@proton.me>
2024-12-29 14:32:35 -0500

    Add files via upload

---
+++ src/app/faq/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Faq from "./Faq";
+
+export const metadata = {
+	title: "FAQ",
+	description: "The Recovera Frequently Asked Questions.",
+};
+
+function Page() {
+	return (
+		<>
+			<Faq />
+		</>
+	);
+}
+
+export default Page;
