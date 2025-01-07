e3dfc3b1a62de6cd0676d9d9fdc37fa32463d012
Anntheresaanode <annatheresanode@proton.me>
2025-01-06 19:17:17 -0500

    Update Certification.js

--- src/app/components/home/Certification.js
+++ src/app/components/home/Certification.js
@@ -13,19 +13,7 @@ import { PageLayout } from "@/app/layout/pageLayout";
 function Certification() {
 	return (
 		<div className="py-20">
-			// <PageLayout>
-			// 	<div className="space-y-10">
-			// 		<h2 data-aos="fade-up" className="text-center">Our Certifications</h2>
-
-			// 		<div className="w-full max-w-3xl mx-auto">
-			// 			<div className="cert flex items-center gap-5 justify-center flex-wrap">
-			// 				{listCert.map((item, index) => (
-			// 					<ListCert key={index} item={item} />
-			// 				))}
-			// 			</div>
-			// 		</div>
-			// 	</div>
-			// </PageLayout>
+			
 		</div>
 	);
 }
